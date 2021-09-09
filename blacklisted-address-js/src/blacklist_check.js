2e82797e4cd6f5d2bfa44828d45bc046bce1dde7
kdawg <kdawg@arbitraryexecution.com>
2021-09-09 19:38:06 -0400

    Added eslint package and performed linting.

--- blacklisted-address-js/src/blacklist_check.js
+++ blacklisted-address-js/src/blacklist_check.js
@@ -1,43 +1,39 @@
-const { Finding, FindingSeverity, FindingType } = require("forta-agent");
-const fs = require("fs");
+const { Finding, FindingSeverity, FindingType } = require('forta-agent');
 
 // TODO: update the blacklist periodically (hourly/daily) by fetching it from an external source
 
 // blacklist.json contains a partial list of actual addresses from the USDT blacklist
 // source:  https://dune.xyz/phabc/usdt---banned-addresses
-const blacklist_data = fs.readFileSync("./src/blacklist.json");
-const blacklist = JSON.parse(blacklist_data);
-console.log("Loaded " + blacklist.usdt_blacklist.length + " banned addresses for USDT");
-
+const blacklist = require('./blacklist.json');
 
 /**
  * Monitor all transactions for any that involve a blacklisted address.
  */
 async function handleTransaction(txEvent) {
   const findings = [];
-  const { from, hash } = txEvent.transaction; 
-  
+  const { from, hash } = txEvent.transaction;
+
   // look at both EOA and contract addresses
   // ensure all addresses are lowercase for matching
   let addresses = Object.keys(txEvent.addresses);
   addresses = addresses.map((x) => x.toLowerCase());
-  
+
   for (let i = 0; i < blacklist.usdt_blacklist.length; i++) {
-    let address = blacklist.usdt_blacklist[i];
+    const address = blacklist.usdt_blacklist[i];
     if (addresses.includes(address)) {
       findings.push(
         Finding.fromObject({
-          name: "Blacklisted Address",
+          name: 'Blacklisted Address',
           description: `Blacklisted address ${address} was involved in a transaction`,
-          alertId: "DEMO-3",
+          alertId: 'DEMO-3',
           type: FindingType.Suspicious,
           severity: FindingSeverity.Low,
           metadata: {
             from,
             hash,
-            address
+            address,
           },
-        })
+        }),
       );
     }
   }
@@ -46,5 +42,5 @@ async function handleTransaction(txEvent) {
 }
 
 module.exports = {
-  handleTransaction
-};  
+  handleTransaction,
+};
--- blacklisted-address-js/src/blacklist_check.spec.js
+++ blacklisted-address-js/src/blacklist_check.spec.js
@@ -1,8 +1,10 @@
-const { TransactionEvent, FindingType, FindingSeverity, Finding } = require("forta-agent");
+const {
+  TransactionEvent, FindingType, FindingSeverity, Finding,
+} = require('forta-agent');
 
-const { handleTransaction } = require("./blacklist_check.js");
+const { handleTransaction } = require('./blacklist_check');
 
-describe("monitor banned addresses", () => {
+describe('monitor banned addresses', () => {
   // function for creating a simulated transaction
   const createTxEvent = ({ from, hash, addrs }) => {
     const type = null;
@@ -18,32 +20,32 @@ describe("monitor banned addresses", () => {
     return new TransactionEvent(type, network, transaction, receipt, traces, addresses, block);
   };
 
-  describe("handleTransaction", () => {
-    it("returns empty findings if the no addresses match the blacklist", async () => {
+  describe('handleTransaction', () => {
+    it('returns empty findings if the no addresses match the blacklist', async () => {
       const txEvent = createTxEvent({
-        from: "0xab5801a7d398351b8be11c439e05c5b3259aec9b",
-        hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
-        addrs: { 
-          "0xab5801a7d398351b8be11c439e05c5b3259aec9b" : true,
-          "0x0000000000000000000000000000000000000001" : true,
-          "0x0000000000000000000000000000000000000002" : true
-        }
+        from: '0xab5801a7d398351b8be11c439e05c5b3259aec9b',
+        hash: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        addrs: {
+          '0xab5801a7d398351b8be11c439e05c5b3259aec9b': true,
+          '0x0000000000000000000000000000000000000001': true,
+          '0x0000000000000000000000000000000000000002': true,
+        },
       });
 
       const findings = await handleTransaction(txEvent);
 
       expect(findings).toStrictEqual([]);
     });
-    
-    it("returns a finding if any address involved in the transaction is blacklisted", async () => {
+
+    it('returns a finding if any address involved in the transaction is blacklisted', async () => {
       const txEvent = createTxEvent({
-        from: "0xab5801a7d398351b8be11c439e05c5b3259aec9b",
-        hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+        from: '0xab5801a7d398351b8be11c439e05c5b3259aec9b',
+        hash: '0x0000000000000000000000000000000000000000000000000000000000000000',
         addrs: {
-          "0xab5801a7d398351b8be11c439e05c5b3259aec9b" : true,
-          "0x0000000000000000000000000000000000000001" : true,
-          "0x68c88fccbc6d5a218711bd897add734aeefd1270" : true
-        }
+          '0xab5801a7d398351b8be11c439e05c5b3259aec9b': true,
+          '0x0000000000000000000000000000000000000001': true,
+          '0x68c88fccbc6d5a218711bd897add734aeefd1270': true,
+        },
       });
 
       const findings = await handleTransaction(txEvent);
@@ -52,18 +54,18 @@ describe("monitor banned addresses", () => {
 
       expect(findings).toStrictEqual([
         Finding.fromObject({
-          name: "Blacklisted Address",
+          name: 'Blacklisted Address',
           description: `Blacklisted address ${address} was involved in a transaction`,
-          alertId: "DEMO-3",
+          alertId: 'DEMO-3',
           type: FindingType.Suspicious,
           severity: FindingSeverity.Low,
           metadata: {
             from,
             hash,
-            address
-          }
-        })
-      ]);   
-    });  
+            address,
+          },
+        }),
+      ]);
+    });
   });
 });
