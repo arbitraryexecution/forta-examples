4511d2e10adbbc90701088806d8485ba70886fa8
kdawg <kdawg@arbitraryexecution.com>
2021-09-09 18:08:51 -0400

    Initial commit of example blacklist check agent.

---
+++ blacklisted-address-js/src/blacklist_check.js
@@ -0,0 +1,50 @@
+const { Finding, FindingSeverity, FindingType } = require("forta-agent");
+const fs = require("fs");
+
+// TODO: update the blacklist periodically (hourly/daily) by fetching it from an external source
+
+// blacklist.json contains a partial list of actual addresses from the USDT blacklist
+// source:  https://dune.xyz/phabc/usdt---banned-addresses
+const blacklist_data = fs.readFileSync("./src/blacklist.json");
+const blacklist = JSON.parse(blacklist_data);
+console.log("Loaded " + blacklist.usdt_blacklist.length + " banned addresses for USDT");
+
+
+/**
+ * Monitor all transactions for any that involve a blacklisted address.
+ */
+async function handleTransaction(txEvent) {
+  const findings = [];
+  const { from, hash } = txEvent.transaction; 
+  
+  // look at both EOA and contract addresses
+  // ensure all addresses are lowercase for matching
+  let addresses = Object.keys(txEvent.addresses);
+  addresses = addresses.map((x) => x.toLowerCase());
+  
+  for (let i = 0; i < blacklist.usdt_blacklist.length; i++) {
+    let address = blacklist.usdt_blacklist[i];
+    if (addresses.includes(address)) {
+      findings.push(
+        Finding.fromObject({
+          name: "Blacklisted Address",
+          description: `Blacklisted address ${address} was involved in a transaction`,
+          alertId: "DEMO-3",
+          type: FindingType.Suspicious,
+          severity: FindingSeverity.Low,
+          metadata: {
+            from,
+            hash,
+            address
+          },
+        })
+      );
+    }
+  }
+
+  return findings;
+}
+
+module.exports = {
+  handleTransaction
+};  
