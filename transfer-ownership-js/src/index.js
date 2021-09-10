13a11647c7813ad3749e0045a2a51835926bf06e
yowl <yowl@arbitraryexecution.com>
2021-09-09 17:07:59 -0700

    initial commit of transfer ownership agent

---
+++ transfer-ownership-js/src/index.js
@@ -0,0 +1,44 @@
+const ethers = require("ethers");
+const BigNumber = require("bignumber.js");
+const { Finding, FindingSeverity, FindingType, getJsonRpcUrl } = require("forta-agent");
+
+const ABI = require("./events.json");
+const EVENTS = ABI.map(ourEvent => ourEvent["name"] + "(" +
+  ourEvent["inputs"].map(input => input["type"]).join(",") + ")");
+
+const provider = new ethers.providers.WebSocketProvider(getJsonRpcUrl());
+
+const handleTransaction = async (txEvent) => {
+  const findings = [];
+  let events;
+
+  EVENTS.forEach(eventProto => {
+    events = txEvent.filterEvent(eventProto);
+    events.forEach(ourEvent => {
+      if (ourEvent.length != 0) {
+        findings.push(
+          Finding.fromObject({
+            name: eventProto.split("(")[0],
+            description: eventProto.split("(")[0],
+            alertId: "FORTA-1",
+            severity: FindingSeverity.Medium,
+            type: FindingType.Suspicious,
+          })
+        );
+      }
+    });
+  });
+
+  return findings;
+};
+
+// const handleBlock = async (blockEvent) => {
+//   const findings: Finding[] = [];
+//   // detect some block condition
+//   return findings;
+// };
+
+module.exports = {
+  handleTransaction,
+  // handleBlock,
+};
