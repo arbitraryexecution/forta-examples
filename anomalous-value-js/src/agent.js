8696e78bcf3ba317b1fb333116e2b6173414d837
rbasri <rsb2158@gmail.com>
2021-10-07 17:19:24 -0400

    updated versions and wrote tests where missing

---
+++ anomalous-value-js/src/agent.js
@@ -0,0 +1,61 @@
+const ethers = require("ethers");
+const BigNumber = require("bignumber.js");
+const RollingMath = require("rolling-math");
+const { Finding, FindingSeverity, FindingType, getJsonRpcUrl } = require("forta-agent");
+
+const contractAddresses = {};
+const provider = new ethers.providers.getDefaultProvider(getJsonRpcUrl());
+
+function provideHandleTransaction(contractAddresses, provider) {
+  return async function handleTransaction(txEvent) {
+    //console.log(contractAddresses[txEvent.to]);
+    const findings = [];
+
+    // skip if transaction is contract creation
+    if (!txEvent.to) {
+      return findings;
+    }
+
+    const value = new BigNumber(txEvent.transaction.value);
+
+    // check if we've seen this address
+    if (contractAddresses[txEvent.to]) {
+      const average = contractAddresses[txEvent.to].getAverage();
+      const standardDeviation = contractAddresses[txEvent.to].getStandardDeviation();
+
+      // if the value is over 5 standard deviations from the mean and
+      // we have a sample size of more than 40, report the finding
+      if (value.isGreaterThan(average.plus(standardDeviation.times(5))) &&
+          contractAddresses[txEvent.to].getNumElements() > 40) {
+        findings.push(
+          Finding.fromObject({
+            name: "High Value",
+            description: `Value: ${value}`,
+            alertId: "AE-ANOMALOUS-VALUE",
+            severity: FindingSeverity.Medium,
+            type: FindingType.Suspicious,
+            metadata: { "contract" : txEvent.to },
+          })
+        );
+      }
+    } else {
+      if (await provider.getCode(txEvent.to) != "0x") {
+        // address is a contract we haven't seen before, initialize it
+        contractAddresses[txEvent.to] = new RollingMath(1000);
+      } else {
+        // externally owned account, return
+        return findings;
+      }
+    }
+
+    // add transaction to contractAddresses dictionary
+    contractAddresses[txEvent.to].addElement(value);
+    return findings;
+  };
+}
+ 
+
+module.exports = {
+  provideHandleTransaction,
+  handleTransaction: provideHandleTransaction(contractAddresses, provider)
+}
